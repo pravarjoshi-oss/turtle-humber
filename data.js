85d350c7cbb5902b81a420cf0ec80cf1d5a6ee43
pravarjoshi-oss <pravar.joshi@torontomu.ca>
2026-07-08 14:37:19 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,518 @@
+const MAP_DATA = {
+  "center": [
+    43.643857435560605,
+    -79.49141285462815
+  ],
+  "bounds": [
+    [
+      43.642667,
+      -79.4920757092563
+    ],
+    [
+      43.6450478711212,
+      -79.49075
+    ]
+  ],
+  "boundary": {
+    "type": "FeatureCollection",
+    "crs": {
+      "type": "name",
+      "properties": {
+        "name": "EPSG:4326"
+      }
+    },
+    "features": [
+      {
+        "type": "Feature",
+        "id": 2,
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [
+            [
+              [
+                -79.4906576903097,
+                43.6425709282059
+              ],
+              [
+                -79.4908027234279,
+                43.6425431210222
+              ],
+              [
+                -79.4908301217739,
+                43.6426364807
+              ],
+              [
+                -79.4908601892325,
+                43.6427379890217
+              ],
+              [
+                -79.4909326062566,
+                43.6429557521494
+              ],
+              [
+                -79.4910947751281,
+                43.6433203847729
+              ],
+              [
+                -79.4911820202627,
+                43.6435184403493
+              ],
+              [
+                -79.4911786570241,
+                43.6435730908965
+              ],
+              [
+                -79.4913770357173,
+                43.6438991924376
+              ],
+              [
+                -79.4917550805052,
+                43.6445278600811
+              ],
+              [
+                -79.4920757092563,
+                43.6450201371105
+              ],
+              [
+                -79.4920498929945,
+                43.6450478711212
+              ],
+              [
+                -79.4919428456136,
+                43.6449367454578
+              ],
+              [
+                -79.4916106079268,
+                43.6444145498918
+              ],
+              [
+                -79.491260789198,
+                43.6438842220045
+              ],
+              [
+                -79.4908849760917,
+                43.6431473642453
+              ],
+              [
+                -79.4907306547768,
+                43.6427561479837
+              ],
+              [
+                -79.4906576903097,
+                43.6425709282059
+              ]
+            ]
+          ]
+        },
+        "properties": {
+          "OBJECTID": 2,
+          "GlobalID": "844793b4-a2cf-4cc3-990c-59cdb228c067"
+        }
+      }
+    ]
+  },
+  "densityAreas": [
+    {
+      "id": 1,
+      "name": "Nest Density Area #1",
+      "ring": [
+        [
+          -79.4912103,
+          43.6434731
+        ],
+        [
+          -79.4912103,
+          43.6436379
+        ],
+        [
+          -79.4910117,
+          43.6436379
+        ],
+        [
+          -79.4910117,
+          43.6434731
+        ],
+        [
+          -79.4912103,
+          43.6434731
+        ]
+      ],
+      "desc": "Several predated nests were found throughout this zone."
+    },
+    {
+      "id": 2,
+      "name": "Nest Density Area #2",
+      "ring": [
+        [
+          -79.4913305,
+          43.643811
+        ],
+        [
+          -79.4914121,
+          43.6439727
+        ],
+        [
+          -79.4912255,
+          43.644022
+        ],
+        [
+          -79.4911439,
+          43.6438603
+        ],
+        [
+          -79.4913305,
+          43.643811
+        ]
+      ],
+      "desc": "Several predated nests were found throughout this zone."
+    }
+  ],
+  "constructionArea": {
+    "name": "Unsurveyed \u2014 Active Construction",
+    "ring": [
+      [
+        -79.4914617,
+        43.64404
+      ],
+      [
+        -79.4917550805052,
+        43.6445278600811
+      ],
+      [
+        -79.4920757092563,
+        43.6450201371105
+      ],
+      [
+        -79.4920498929945,
+        43.6450478711212
+      ],
+      [
+        -79.4919428456136,
+        43.6449367454578
+      ],
+      [
+        -79.4916106079268,
+        43.6444145498918
+      ],
+      [
+        -79.4913635,
+        43.64404
+      ],
+      [
+        -79.4914617,
+        43.64404
+      ]
+    ],
+    "desc": "We were unable to survey this area due to active construction. It is highly likely that even this stretch of shoreline is a hotspot for turtle nests."
+  },
+  "points": [
+    {
+      "id": 2,
+      "lat": 43.642667,
+      "lng": -79.49075,
+      "type": "predated",
+      "title": "Predated Turtle Nest",
+      "desc": "A predated turtle nest \u2014 eggs dug up and eaten by a predator, leaving scattered eggshells. Documented ~1:46 PM.",
+      "media": [
+        {
+          "t": "photo",
+          "src": "assets/2-Nest.jpg",
+          "cap": "The nest"
+        },
+        {
+          "t": "photo",
+          "src": "assets/2-Eggshell.jpg",
+          "cap": "Eggshell remains"
+        }
+      ]
+    },
+    {
+      "id": 3,
+      "lat": 43.642833,
+      "lng": -79.490833,
+      "type": "slope",
+      "title": "Graduated Slope to Water",
+      "desc": "A gentle, natural slope between land and water \u2014 the kind of gradual ramp turtles can use to safely climb ashore to nest and return to the water.",
+      "media": []
+    },
+    {
+      "id": 4,
+      "lat": 43.642972,
+      "lng": -79.490889,
+      "type": "predated",
+      "title": "Multiple Predated Turtle Nests",
+      "desc": "Several predated turtle nests clustered together, eggshells scattered across the ground. Documented ~1:51 PM.",
+      "media": [
+        {
+          "t": "photo",
+          "src": "assets/4-Nest.jpg",
+          "cap": "The nests"
+        },
+        {
+          "t": "photo",
+          "src": "assets/4-Eggshells.jpg",
+          "cap": "Eggshell remains"
+        },
+        {
+          "t": "photo",
+          "src": "assets/4-ZoomedOutView.jpg",
+          "cap": "Zoomed-out view of the area"
+        }
+      ]
+    },
+    {
+      "id": 5,
+      "lat": 43.643083,
+      "lng": -79.490917,
+      "type": "slope",
+      "title": "Graduated Slope to Water",
+      "desc": "Another gentle, natural slope to the water \u2014 a potential safe access point for nesting turtles. Documented ~1:52 PM.",
+      "media": [
+        {
+          "t": "photo",
+          "src": "assets/5-SlopeToWater.jpg",
+          "cap": "Slope down to the water"
+        }
+      ]
+    },
+    {
+      "id": 8,
+      "lat": 43.643694,
+      "lng": -79.491222,
+      "type": "protected",
+      "title": "Protected & Excavated Snapping Turtle Nest",
+      "desc": "We encountered a snapping turtle nesting here during the survey and protected the nest. It was later excavated \u2014 39 eggs recovered and sent to incubation.",
+      "media": [
+        {
+          "t": "photo",
+          "src": "assets/8-TurtleWhoNested.jpg",
+          "cap": "The snapping turtle that nested here"
+        },
+        {
+          "t": "photo",
+          "src": "assets/8-NestProtector.jpg",
+          "cap": "Nest protector installed over the nest"
+        },
+        {
+          "t": "photo",
+          "src": "assets/9-ExcavatedEggs.jpg",
+          "cap": "Excavated eggs \u2014 39 recovered for incubation"
+        }
+      ]
+    }
+  ],
+  "densityPins": [
+    {
+      "lat": 43.6435103,
+      "lng": -79.491118,
+      "area": 1,
+      "media": [
+        {
+          "t": "video",
+          "src": "assets/67-8.mp4",
+          "poster": "assets/67-8-poster.jpg"
+        }
+      ]
+    },
+    {
+      "lat": 43.6435223,
+      "lng": -79.4910938,
+      "area": 1,
+      "media": [
+        {
+          "t": "photo",
+          "src": "assets/67-4.jpg"
+        }
+      ]
+    },
+    {
+      "lat": 43.6435269,
+      "lng": -79.4911246,
+      "area": 1,
+      "media": [
+        {
+          "t": "photo",
+          "src": "assets/67-3.jpg"
+        }
+      ]
+    },
+    {
+      "lat": 43.6435331,
+      "lng": -79.4911468,
+      "area": 1,
+      "media": [
+        {
+          "t": "video",
+          "src": "assets/67-9.mp4",
+          "poster": "assets/67-9-poster.jpg"
+        }
+      ]
+    },
+    {
+      "lat": 43.6435404,
+      "lng": -79.4911216,
+      "area": 1,
+      "media": [
+        {
+          "t": "photo",
+          "src": "assets/67-6.jpg"
+        }
+      ]
+    },
+    {
+      "lat": 43.643546,
+      "lng": -79.4911511,
+      "area": 1,
+      "media": [
+        {
+          "t": "photo",
+          "src": "assets/67-7.jpg"
+        }
+      ]
+    },
+    {
+      "lat": 43.6435687,
+      "lng": -79.4911065,
+      "area": 1,
+      "media": [
+        {
+          "t": "video",
+          "src": "assets/67-10.mp4",
+          "poster": "assets/67-10-poster.jpg"
+        }
+      ]
+    },
+    {
+      "lat": 43.6435712,
+      "lng": -79.4911521,
+      "area": 1,
+      "media": [
+        {
+          "t": "photo",
+          "src": "assets/67-5.jpg"
+        }
+      ]
+    },
+    {
+      "lat": 43.6435824,
+      "lng": -79.491165,
+      "area": 1,
+      "media": [
+        {
+          "t": "photo",
+          "src": "assets/67-1.jpg"
+        }
+      ]
+    },
+    {
+      "lat": 43.6435947,
+      "lng": -79.4911807,
+      "area": 1,
+      "media": [
+        {
+          "t": "photo",
+          "src": "assets/67-2.jpg"
+        }
+      ]
+    },
+    {
+      "lat": 43.6438639,
+      "lng": -79.4912681,
+      "area": 2,
+      "media": [
+        {
+          "t": "photo",
+          "src": "assets/1011-4.jpg"
+        }
+      ]
+    },
+    {
+      "lat": 43.6438604,
+      "lng": -79.4912553,
+      "area": 2,
+      "media": [
+        {
+          "t": "photo",
+          "src": "assets/1011-6.jpg"
+        }
+      ]
+    },
+    {
+      "lat": 43.6438966,
+      "lng": -79.4913113,
+      "area": 2,
+      "media": [
+        {
+          "t": "photo",
+          "src": "assets/1011-3.jpg"
+        }
+      ]
+    },
+    {
+      "lat": 43.6439034,
+      "lng": -79.4912803,
+      "area": 2,
+      "media": [
+        {
+          "t": "photo",
+          "src": "assets/1011-5.jpg"
+        }
+      ]
+    },
+    {
+      "lat": 43.6439185,
+      "lng": -79.4913168,
+      "area": 2,
+      "media": [
+        {
+          "t": "photo",
+          "src": "assets/1011-2.jpg"
+        }
+      ]
+    },
+    {
+      "lat": 43.6439291,
+      "lng": -79.4913055,
+      "area": 2,
+      "media": [
+        {
+          "t": "photo",
+          "src": "assets/1011-9.jpg"
+        }
+      ]
+    },
+    {
+      "lat": 43.6439216,
+      "lng": -79.4913336,
+      "area": 2,
+      "media": [
+        {
+          "t": "photo",
+          "src": "assets/1011-8.jpg"
+        }
+      ]
+    },
+    {
+      "lat": 43.6439573,
+      "lng": -79.4913159,
+      "area": 2,
+      "media": [
+        {
+          "t": "photo",
+          "src": "assets/1011-1.jpg"
+        }
+      ]
+    },
+    {
+      "lat": 43.6439537,
+      "lng": -79.4913633,
+      "area": 2,
+      "media": [
+        {
+          "t": "photo",
+          "src": "assets/1011-7.jpg"
+        }
+      ]
+    }
+  ]
+};
